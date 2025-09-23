9c61ce1560a554a05dbb896b107138a2c4f2d886
MarkoTrajkovic <1531963@learn.vsb.bc.ca>
2025-09-23 21:48:42 +0000

    coding 1-3 2.3

--- 2.3/coding3.py
+++ 2.3/coding3.py
@@ -25,3 +25,24 @@ Would you like fries for $3? (Yes/No)
 > yes
 Your total is $3.42
 """
+burger_cost = 0
+fries_cost = 0
+
+print("Would you like a burger for $5?")
+burger = input().lower()
+print("Would you like fries for $3?")
+fries = input().lower()
+
+if burger == "yes":
+    burger_cost = 5
+elif burger == "no":
+    burger_cost = 0
+
+if fries == "yes":
+    fries_cost = 3
+elif fries == "no":
+    fries_cost = 0
+
+total_cost = (burger_cost + fries_cost) * 1.14
+
+print("Your total is: " + str(total_cost))
