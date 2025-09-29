68fa6f490e5263f3a71146b72e6de5df9e4b4725
MarkoTrajkovic <1531963@learn.vsb.bc.ca>
2025-09-29 21:45:31 +0000

    first

--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -6,3 +6,16 @@ You must use user input to add interactivity to the program.
 You must design your algorithm in English first, then translate it to Python code.
 Test as you go! Describe in your comments what steps you took to test your code.
 """
+#my program will use data from the class and find the classes favourite sport and how popular it is in a percentage, and the total average
+#of the classes favouite digits.
+
+file = open(responses.csv)
+
+print("What is your name?")
+name = input()
+
+for line in file:
+    if name in line:
+        users_fav = line.split(",")
+        print("Hello, " + name + ". Your favourite sport is " + line[4] + ". Do you want to know the favourite of the class?")
+
