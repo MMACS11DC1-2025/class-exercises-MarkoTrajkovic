d9d9f806a87979d6bfe2699334842c998e471a2f
MarkoTrajkovic <1531963@learn.vsb.bc.ca>
2025-10-08 21:15:19 +0000

    febhfbe

--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -6,6 +6,7 @@ and respond to the user depending on how many people like the sport.
 #information from file
 file = open("2.4/responses.csv")
 
+#initializing variable for 3rd question
 similarsport = 0
 
 #finds name of user
@@ -21,10 +22,12 @@ for line in file:
     else:
         continue
 
+#asks for new name
 print("Who would you like to know their favourite sport?")
 answer = input().lower()
 file = open("2.4/responses.csv")
-    
+
+#Tells the new persons favourite sports to watch
 for line in file:
     user_fav = line.split(",")
     if answer in line.lower():
@@ -32,20 +35,34 @@ for line in file:
         print(user_fav[1] + "'s favourite sport to watch is " + user_fav[6] + ".")
         break
 
-print("Do you know how many people also like to watch or play that?")
+#asks you if you would like to know how many people like to watch or play sport from the new person
+print("Do you know how many people also like to watch and/or play that?")
 answer2 = input().lower()
-    
+
+#if the answer is yes, will add 1 to a tally when the sport comes up in any line from the file
 for line in file:
     if answer2 == "yes":
         file = open("2.4/responses.csv")
         if str(user_fav[6]) in line:
             similarsport += 1
-print("Exactly " + str(similarsport) + " people like to watch and/or play " + user_fav[6] + "!")
-#matches other persons name with data set, tells favourite sport to watch
-if similarsport >= 5:
-    print("Wow! Thats a popular sport!")
-elif similarsport < 5:
-    print("Not a very popular sport. " + answer + " likes a niche sport!")
+#if answer is no, respond approprietly and stop the loop
+    elif answer2 == "no":
+        print("Oh. Ok.")
+        break
+#if answer is not yes or know, respond approprietly and stop the loop
+    else:
+        print("Sorry, im not sure what you mean.")
+        break
+
+#only if the answer is yes, the program will tell you how many instances of the sport shows up in the dataset.
+if answer2 == "yes":
+    print("Exactly " + str(similarsport) + " people like to watch and/or play " + user_fav[6] + "!")
+#if the sport appears more than 5 times, it will respond appropriely
+    if similarsport >= 5:
+        print("Wow! Thats a popular sport!")
+#if the sport appears less than 5 times, it will respond appropriely
+    elif similarsport < 5:
+        print("Not a very popular sport. " + answer + " likes a niche sport!")
         
             
 
