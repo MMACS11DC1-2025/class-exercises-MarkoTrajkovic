74534872e93e505980ed83684b01a5f7b583f7e2
MarkoTrajkovic <1531963@learn.vsb.bc.ca>
2025-10-15 22:03:54 +0000

    esef

--- 2.4/main.py
+++ 2.4/main.py
@@ -1,1 +1,22 @@
 file = open("2.4/responses.csv")
+
+
+import turtle
+t = turtle.Turtle()
+
+answer = input("Want to see a bunch of stars?")
+while True:
+	def staircase():
+		t.pendown()
+		t.right(-70)
+		t.forward(200)
+		t.right(140)
+		t.forward(200)
+		t.right(150)
+		t.forward(200)
+		t.right(140)
+		t.forward(185)
+		t.right(145)
+		t.forward(200)
+		t.penup()
+	staircase();
