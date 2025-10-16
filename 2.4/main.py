b5b8fd2a5c61ea5c2f382a436c685e84fe0dcf2b
MarkoTrajkovic <1531963@learn.vsb.bc.ca>
2025-10-16 20:53:46 +0000

    ddrhhr

--- 2.4/main.py
+++ 2.4/main.py
@@ -4,19 +4,26 @@ file = open("2.4/responses.csv")
 import turtle
 t = turtle.Turtle()
 
-answer = input("Want to see a bunch of stars?")
+answer = input("Want to see a cool star pattern?")
+size = int(input("How big do you want the stars to be?"))
+x = int(input("Where do you want the stars to start? (x axis)"))
+y = int(input("Where do you want the stars to start? (y axis)"))
+
 while True:
-	def staircase():
+	def stars():
+		t.penup()
+		t.goto(x,y)
 		t.pendown()
 		t.right(-70)
-		t.forward(200)
+		t.forward(size)
 		t.right(140)
-		t.forward(200)
+		t.forward(size)
 		t.right(150)
-		t.forward(200)
+		t.forward(size)
 		t.right(140)
-		t.forward(185)
+		t.forward(size)
 		t.right(145)
-		t.forward(200)
+		t.forward(size + 15)
 		t.penup()
-	staircase();
+    
+	stars();
