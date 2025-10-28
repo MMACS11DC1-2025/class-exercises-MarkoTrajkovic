9b91305b52b18d9e5a682c29c5a761455fcf30ba
MarkoTrajkovic <1531963@learn.vsb.bc.ca>
2025-10-28 22:00:37 +0000

    gffh

--- 4.4/save.py
+++ 4.4/save.py
@@ -1,19 +1,20 @@
 import turtle
 
 turt = turtle.Turtle()
-
+turt.speed(0)
 size = int(input("What size is the starting fractal?"))
 
 
-while size >= 0:
-  def shape(size):
-    turt.goto(0,0)
-    turt.pendown()
-    turt.circle(20+size)
-    turt.forward(50+size)
-    turt.left(40)
-    turt.forward(20+size)
-    if size <= 0:
-      return 0
-  shape(size)
+
+def shape(size):
+  turt.goto(0,0)
+  turt.pendown()
+  turt.circle(20+size)
+  turt.forward(50+size)
+  turt.left(40)
+  turt.forward(20+size)
   size -= 1
+  if size <= 0:
+    return 0
+  shape(size)
+shape(size)
