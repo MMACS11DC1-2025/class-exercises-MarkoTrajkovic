585cf417c1915d27649e40fccf95d5df2e121e37
MarkoTrajkovic <1531963@learn.vsb.bc.ca>
2025-11-14 21:33:59 +0000

    fghty

--- 5.1/image_open.py
+++ 5.1/image_open.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
-image_green = Image.open("CS11/5.1/kid-green.jpg").load()
-image_beach = Image.open("CS11/5.1/beach.jpg").load()
+image_green = Image.open("5.1/kid-green.jpg").load()
+image_beach = Image.open("5.1/beach.jpg").load()
 
 def is_green(r, g, b):
     if r < 25 and r >= 0 and g > 230 and g <= 255 and b < 25 and b >= 0:
